1ccd8637cda5691fd6615ddcbc705fa145314bac
Tanner Filip <tannerfilip@gmail.com>
2013-09-02 15:37:26 -0500

    initial commit

---
+++ site.pp
@@ -0,0 +1,20 @@
+#Make sure sudoers is has the right owner/mode
+file { "/ect/sudoers":
+	owner => root, group => root, mode => 400
+}
+#Purge existing Puppet firewall rules
+resources { "firewall":
+  purge => true
+}
+
+#Make sure pre/post classes are run in the right order
+Firewall {
+  before  => Class['firewall::post'],
+  require => Class['firewall::pre'],
+}
+#Define pre/post classes
+class { ['firewall::pre', 'firewall::post']: }
+
+#Make sure correct packages are installed
+class { 'firewall': }
+
