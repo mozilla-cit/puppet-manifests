89baf7250c4d96defd0e87a74f0f6ef4c19bf7b1
Justin Dow <justin@dowhaus.com>
2013-09-04 06:36:16 -0700

    Moving site.pp to top level manifests directory

---
+++ manifests/site.pp
@@ -0,0 +1,23 @@
+#Make sure sudoers is has the right owner/mode
+file { '/etc/sudoers':
+  owner => 'root',
+  group => 'root',
+  mode  => '0400'
+}
+
+#Purge existing Puppet firewall rules
+resources { 'firewall':
+  purge => true
+}
+
+#Make sure pre/post classes are run in the right order
+Firewall {
+  before  => Class['firewall::post'],
+  require => Class['firewall::pre'],
+}
+#Define pre/post classes
+class { ['firewall::pre', 'firewall::post']: }
+
+#Make sure correct packages are installed
+class { 'firewall': }
+
