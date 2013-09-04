1a506fef5278d97f3c0cbe3a60b8b64df652d563
Tanner Filip <tannerfilip@gmail.com>
2013-09-04 17:53:15 -0500

    missing spaces

--- manifests/nodes/init.pp
+++ manifests/nodes/init.pp
@@ -2,10 +2,10 @@ node default {
 	include base
 	include users
 }
-node webservers inherits default{
+node webservers inherits default {
 	include nginx
 }
-node  inherits default{ #TODO: Add hosts for each Discourse host
+node  inherits default { #TODO: Add hosts for each Discourse host
 	include redis
 }
 
