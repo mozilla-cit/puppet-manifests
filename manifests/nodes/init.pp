9fc2cb4fcfbb3eb72be2ae27f49273536f4bd93d
Tanner Filip <tannerfilip@gmail.com>
2013-09-04 17:51:23 -0500

    make this a bit better

--- manifests/nodes/init.pp
+++ manifests/nodes/init.pp
@@ -1,10 +1,11 @@
 node default {
 	include base
 	include users
+}
+node webservers inherits default{
 	include nginx
 }
-
-node  { #TODO: Add hosts for each Discourse host
+node  inherits default{ #TODO: Add hosts for each Discourse host
 	include redis
 }
 
