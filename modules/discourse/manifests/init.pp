bfac199b4db8c45afb533284f8663a9f95b91175
Tanner Filip <tannerfilip@gmail.com>
2013-09-11 18:11:44 -0500

    move package array

--- modules/discourse/manifests/init.pp
+++ modules/discourse/manifests/init.pp
@@ -1,12 +1,13 @@
 #Set up dependencies for Discourse
 class discourse {
+  #Set up package array
+  $discoursepackages = [ 'build-essentials', 'libssl-dev','libyaml-dev', 'git',
+  'libtools', 'libxslt-dev', 'libpq-dev', 'gawk', 'curl', 'pngcrush',
+  'imagemagick', 'python-software-properties' ]
   include base
   include nginx
   include redis
   #  include postgres
-  $discoursepackages = [ 'build-essentials', 'libssl-dev','libyaml-dev', 'git',
-  'libtools', 'libxslt-dev', 'libpq-dev', 'gawk', 'curl', 'pngcrush',
-  'imagemagick', 'python-software-properties' ]
   package { $discoursepackages:
     ensure => 'latest'
   }
