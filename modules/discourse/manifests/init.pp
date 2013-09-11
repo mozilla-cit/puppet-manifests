8182c0e9d6ab9b44fec181f0c14d588739ff03ea
Tanner Filip <tannerfilip@gmail.com>
2013-09-11 17:56:07 -0500

    this is a bit nicer

--- modules/discourse/manifests/init.pp
+++ modules/discourse/manifests/init.pp
@@ -3,42 +3,11 @@ class discourse {
   include nginx
   include redis
   #  include postgres
-  package { 'build-essential':
-    ensure => latest
-  }
-  package { 'libssl-dev':
-    ensure => latest
-  }
-  package { 'libyaml-dev':
-    ensure => latest
-  }
-  package { 'git':
-    ensure => latest
-  }
-  package { 'libtools':
-    ensure => latest
-  }
-  package { 'libxslt-dev':
-    ensure => latest
-  }
-  package { 'libpq-dev':
-    ensure => latest
-  }
-  package { 'gawk':
-    ensure => latest
-  }
-  package { 'curl':
-    ensure => latest
-  }
-  package { 'pngcrush':
-    ensure => latest
-  }
-  package { 'imagemagick':
-    ensure => latest
-  }
-  package { 'python-software-properties':
-    ensure => latest
+  $discoursepackages = [ 'build-essentials', 'libssl-dev','libyaml-dev',  'git', 'libtools', 'libxslt-dev', 'libpq-dev', 'gawk', 'curl', 'pngcrush', 'imagemagick', 'python-software-properties' ]
+  package { $discoursepackages:
+    ensure => 'latest'
   }
 }
+
 
 
