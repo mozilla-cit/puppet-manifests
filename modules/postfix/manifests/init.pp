d3ae8432eaab59a339232291a024d4974aa60385
Tanner Filip <tannerfilip@gmail.com>
2013-09-22 14:11:13 -0500

    lintify

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -5,12 +5,12 @@ class { 'postfix':
     ensure => 'latest'
   }
   file_line { 'required_postfix_settings':
-    path => '/etc/postfix/main.cf',
-    line => ['smtp_sasl_auth_enable = yes',
+    path   => '/etc/postfix/main.cf',
+    line   => ['smtp_sasl_auth_enable = yes',
     'smtp_sasl_password_maps = hash:/etc/postfix/sasl/passwd',
     'smtp_sasl_security_options = noanonymous',
     'smtp_use_tls = yes',
-    'relayhost = [smtp.mandrillapp.com']
+    'relayhost = [smtp.mandrillapp.com'],
     ensure => 'present'
   }
 }
