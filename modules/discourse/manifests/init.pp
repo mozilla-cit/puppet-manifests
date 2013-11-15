a3326e76e0c66a850b1b4985c792fe934c0d62e8
Thomas Farrow <moztaddle@gmail.com>
2013-11-15 20:15:01 +0000

    I (think) that this should make discourse an actual module, and installable. If I'm not mistaken, it was just a manifest before

--- modules/discourse/manifests/init.pp
+++ modules/discourse/manifests/init.pp
@@ -4,17 +4,9 @@ class discourse {
   $discoursepackages = [ 'build-essentials', 'libssl-dev','libyaml-dev', 'git',
   'libtools', 'libxslt-dev', 'libpq-dev', 'gawk', 'curl', 'pngcrush',
   'imagemagick', 'python-software-properties' ]
-  include base
-  include nginx
-  include redis
   #  include postgres
   package { $discoursepackages:
-    ensure  => 'latest'
-  }
-  git::repo{'discourse':
-    path    => '/var/www/', #Placeholder location for now
-    source  => 'git://github.com/discourse/discourse.git',
-    git_tag => 'latest-release'
+    ensure => 'latest'
   }
 }
 
