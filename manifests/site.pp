eb760e710e0775e18ae5b05d37617ff1ce7b9b27
Tom Farrow [:Tad] <tom@mozilla.org.uk>
2014-02-20 17:06:24 +0000

    including runstages before I call it

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,9 @@
+include runstages
 class base {
   include stdlib
-  include runstages
   include users
   include sudo
 }
-
-
 node 'csa-bastion2' {
 	class { 'base' : stage => prep }
 	realize Users::Account['tad']
