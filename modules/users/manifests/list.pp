a54ad1a09288cbe1f94ce10a76cacdad34e2ebef
Tom Farrow [:Tad] <tom@mozilla.org.uk>
2014-02-20 15:48:22 +0000

    set a password hash for tad

--- modules/users/manifests/list.pp
+++ modules/users/manifests/list.pp
@@ -10,7 +10,7 @@ class users::list {
     ingroups => $admingroups,
     authorizedkey => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC+CplsmiLlLzv432y5FD4vQugqsE65pTCcqRIledrLgtsHrLyG8A/aeUhN+lYMSyRZF1yvJgYzxHCct5bDide78G46mFG0al1U082+tSPAnkexP/DKFCDuVZSeuuvpD4W+zxxdhPTGoshcoMw7862m5vstMUCcJoSBXrqBZ2zmnmKWM0HoNGuPjQDrebaUd6fNKcGT5YfSB+ItJAN3wGNdLNG3GzN7NN7xhmH59GaAfPXYzRBq5Oees0c87qHphTiHzUzKP1X9sY39aqy6wGccNjhK2g6Jee2LYSyUcQL8C5gEsag3aA3PAj4hzM/9DyjiIsD79p/IukAnL/woJS8V',
     keycomment => 'tom@mozilla.org.uk',
-    password   => '$6$v/npsjtR90fRz5xw$uqABwvaSef9EP69ozguD.SNJ1jMDZ2ttuuyluBnjcoG6hl6w4nx5jV3h3TVT8e40DcthmzO05h2G9zBAt8mfe0',
+    password   => '$1$QOIxg24o$YVfxxtX0PPFLB/pb5mQ4r.',
   }
 
   #@account { 'root':
