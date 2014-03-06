c6a4e785b7421fa43f46eb51e955c6f95968071b
Tom Farrow <tom@mozilla.org.uk>
2014-03-06 07:41:28 +0000

    made the motd module a class. stupid mistake

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,7 +1,9 @@
-file {'/etc/motd':
-      ensure  => present,
-      mode    => 0644,
-      owner   => root,
-      group   => root,
-      source => "puppet:///modules/motd/motd"
+class motd {
+	file {'/etc/motd':
+      	ensure  => present,
+      	mode    => 0644,
+      	owner   => root,
+      	group   => root,
+      	source => "puppet:///modules/motd/motd"
     }
+}
