f979143ea7226ed3c4c2dab2d379f341a4acc626
Tom Farrow <tom@mozilla.org.uk>
2014-04-26 16:25:57 +0100

    added a monitoring agent module

---
+++ modules/monitoring-agent/manifests/config.pp
@@ -0,0 +1,14 @@
+# Class: monitoring-agent::config
+# Configures the monitoring agent.
+#
+class monitoring-agent::config {
+
+	file { "/etc/xinetd.d/check_mk":
+   		mode => 400,
+    	owner => root,
+    	group => root,
+    	source => "puppet:///modules/monitoring-agent/check_mk",
+    	notify  => Service["xinetd"],
+	}
+
+}
