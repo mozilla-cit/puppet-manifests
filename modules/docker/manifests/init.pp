428d3421d42cb5482a1b31c200dbda762dda32df
Tanner Filip <tannerfilip@gmail.com>
2014-04-27 14:49:32 -0500

    maybe this will work?

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -4,15 +4,15 @@ class docker::update {
       command => "/usr/bin/apt-get update",
     }
 }
-
 class docker::install {
 
-    apt::source { 'docker_repo':
-        location   => 'https://get.docker.io/ubuntu',
-        repos      => 'docker main', 
+    apt::key { 'docker_key':
         key_server => 'keyserver.ubuntu.com',
         key        =>  'A88D21E9',
     }
+    exec { 'add docker repo':
+        command => "/bin/sh -c \"echo deb https://get.docker.io/ubuntu docker main\
+        > /etc/apt/sources.list.d/docker.list\""
     package { "linux-image-generic-lts-raring":
         ensure => present,
     }
