da7476f6dd3992850d9f33bab0653a8a3b07432e
Tanner Filip <tannerfilip@gmail.com>
2014-04-26 20:28:10 -0500

    working run stages?

--- modules/discourse-docker/manifests/init.pp
+++ modules/discourse-docker/manifests/init.pp
@@ -2,8 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+stage { 'first':
+  before => Stage['main'],
+}
+stage { 'last': }
+Stage['main'] -> Stage['last']
+
+
 # Class: discourse-docker
-class discourse-docker {
+class discourse-docker-clone {
+    stage => first,
     # Ensure git is installed
     package { "git":
       ensure => installed,
@@ -13,10 +21,17 @@ class discourse-docker {
   exec { "clone discourse-docker repo":
     command => "/usr/bin/git clone https://github.com/discourse/discourse_docker.git /var/docker"
     }
+}
 
+class discourse-docker-config {
+    stage => main,
 # Copy config
 include 'discourse-docker::app'
 
+}
+
+class discourse-docker-setup {
+    stage => last,
 # Bootstrap app
   exec { "bootstrap Discourse":
     command => '/var/docker/launcher bootstrap app'
