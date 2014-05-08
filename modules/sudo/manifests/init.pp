6525c4709d0961d56db6debb3ba5b868aea46d80
Logan Rosen <loganrosen@gmail.com>
2014-05-07 23:29:44 -0400

    linting of the sudo

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,13 +1,13 @@
+# All the sudo stuff
 class sudo {
+  group { 'plesssudo':
+    ensure => 'present',
+  }
 
-group { "plesssudo":
-    ensure => "present",
-}
-
-file { "/etc/sudoers":
-    mode => 440,
-    owner => root,
-    group => root,
-    source => "puppet:///modules/sudo/sudoers"
-}
+  file { '/etc/sudoers':
+    mode   => '0440',
+    owner  => root,
+    group  => root,
+    source => 'puppet:///modules/sudo/sudoers'
+  }
 }
