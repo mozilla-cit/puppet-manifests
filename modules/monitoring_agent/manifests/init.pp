6bfae3b074b52a7080419e0b2ea58f187bc02320
Logan Rosen <loganrosen@gmail.com>
2014-05-07 23:04:15 -0400

    clean up monitoring_agent with puppet-lint

---
+++ modules/monitoring_agent/manifests/init.pp
@@ -0,0 +1,25 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Installs xinetd, check-mk agent and configures
+class monitoring_agent {
+  # Install xinetd
+  package { 'xinetd':
+    ensure => installed,
+  }
+
+  # Install check_mk agent reccomendations
+  package { 'ethtool':
+    ensure => installed,
+  }
+
+  package { 'smartmontools':
+    ensure => installed,
+  }
+
+  # Install check_mk agent
+  package { 'check-mk-agent':
+    ensure => installed,
+  }
+}
