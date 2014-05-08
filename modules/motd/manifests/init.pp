950c4059d15304b4def986d5f641cdfd4dc5af0a
Logan Rosen <loganrosen@gmail.com>
2014-05-08 18:15:40 -0400

    make the motd dynamic

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,6 +1,11 @@
 # Install a custom MOTD file
 class motd {
-  file {'/etc/motd':
+  file { '/etc/motd':
+    ensure => link,
+    target => '/var/run/motd',
+  }
+
+  file {'/etc/motd.tail':
   ensure  => present,
   mode    => '0644',
   owner   => root,
