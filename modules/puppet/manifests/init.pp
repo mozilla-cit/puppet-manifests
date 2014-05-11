acd506fc858ad734facb43ba5f20e9558877f488
Logan Rosen <loganrosen@gmail.com>
2014-05-10 23:59:34 -0400

    fix various issues

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,13 +1,14 @@
 # Installs the latest Puppet from the apt repo
 class puppet {
+  include apt
   apt::source { 'puppetlabs':
     location   => 'https://apt.puppetlabs.com',
-    repos      => 'main',
+    repos      => 'main dependencies',
     key        => '4BD6EC30',
     key_server => 'pgp.mit.edu',
   }
 
-  package { 'puppet':
+  package { 'puppet-common':
     ensure  => latest,
     require => Apt::Source[puppetlabs]
   }
