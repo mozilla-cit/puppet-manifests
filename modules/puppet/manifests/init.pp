987f146e46f83f6ce3f40bd442645fbf3a082ce0
Logan Rosen <loganrosen@gmail.com>
2014-05-10 23:49:48 -0400

    put puppet in a class

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,12 +1,14 @@
 # Installs the latest Puppet from the apt repo
-apt::source { 'puppetlabs':
-  location   => 'https://apt.puppetlabs.com',
-  repos      => 'main',
-  key        => '4BD6EC30',
-  key_server => 'pgp.mit.edu',
-}
+class puppet {
+  apt::source { 'puppetlabs':
+    location   => 'https://apt.puppetlabs.com',
+    repos      => 'main',
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
 
-package { 'puppet':
-  ensure  => latest,
-  require => Apt::Source[puppetlabs]
+  package { 'puppet':
+    ensure  => latest,
+    require => Apt::Source[puppetlabs]
+  }
 }
