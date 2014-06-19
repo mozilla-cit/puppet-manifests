640e82bd5099753ddc9c8c17a8fc5e359271b23f
Scott Lee <scott.leea@gmail.com>
2014-06-19 18:16:36 -0400

    [T78] Added OMD modules for Ubuntu Preceise (12.04).

--- modules/monitoring_agent/manifests/init.pp
+++ modules/monitoring_agent/manifests/init.pp
@@ -2,8 +2,39 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Installs OMD for Ubuntu 12.04
 # Installs xinetd, check-mk agent and configures
+
 class monitoring_agent {
+  exec { 'gpg_receive':
+    command => 'gpg --keyserver keys.gnupg.net --recv-keys F8C1CA08A57B9ED7',
+    path    => ['/usr/bin', '/bin'],
+  } 
+
+  exec { 'gpg_add':
+    command => 'gpg --armor --export F8C1CA08A57B9ED7 | apt-key add -',
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['gpg_receive']
+  } 
+
+  exec { 'sources':
+    command => 'echo "deb http://labs.consol.de/repo/stable/ubuntu precise main" >> /etc/apt/sources.list',
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['gpg_add']
+  } 
+
+  exec { 'update':
+    command => 'apt-get update',
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['sources']
+  } 
+
+  exec { 'install':
+    command => 'apt-get install -y omd',
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['update']
+  } 
+
   # Install xinetd
   package { 'xinetd':
     ensure => installed,
