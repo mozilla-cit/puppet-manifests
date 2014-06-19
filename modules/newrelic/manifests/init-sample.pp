d2bba0dd227f9122d03e00c407183488aeff95e8
Tom Farrow <tom@mozilla.org.uk>
2014-06-19 16:53:18 +0100

    made newrelic a real class

---
+++ modules/newrelic/manifests/init-sample.pp
@@ -0,0 +1,6 @@
+class newrelic {
+
+	class { 'nrsysmond':
+    	license_key    => '0123456789abcdef0123456789abcdef01234567',
+  	}
+}
--- modules/nr-config/manifests/init-sample.pp
+++
@@ -1,4 +0,0 @@
-#Sample New Relic config
-class { 'nrsysmond':
-    license_key    => '0123456789abcdef0123456789abcdef01234567',
-  }
