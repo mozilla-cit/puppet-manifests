d7b9f1a77ad070e722fd08dde6261766d02162e3
Scott Lee <scott.leea@gmail.com>
2014-07-11 09:08:44 -0400

    Added comments to the sudo module.

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,9 +1,14 @@
-# All the sudo stuff
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class sudo {
+  # Checks that the plessudo group exists
   group { 'plesssudo':
     ensure => 'present',
   }
 
+  # Sets ownership and file permissions for the sudoers file
   file { '/etc/sudoers':
     mode   => '0440',
     owner  => root,
