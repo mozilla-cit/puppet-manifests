88f37a5fe48e3a4358bd5beeeb14cd2ebf85594f
Scott Lee <scott.leea@gmail.com>
2014-07-11 09:04:17 -0400

    Added comments to the nginx module.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,8 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class nginx {
+  # Installs the nginx package
   package { 'nginx':
     ensure => installed,
   }
 
+  # Starts nginx
   service { 'nginx':
     ensure  => running,
     require => Package['nginx'],
